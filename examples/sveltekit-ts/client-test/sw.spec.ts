7536dc223598c2665b753811acf33b1e61ce0e8a
userquin <userquin@gmail.com>
2023-05-08 19:20:10 +0200

    chore: use `Promise.race` when awaiting sw registration

--- examples/sveltekit-ts/client-test/sw.spec.ts
+++ examples/sveltekit-ts/client-test/sw.spec.ts
@@ -5,10 +5,13 @@ test('The service worker registers and precaches', async ({ page}) => {
     await page.goto('/');
 
     const swURL = await page.evaluate(async () => {
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
-        const registration = await navigator.serviceWorker.ready;
-        return registration.active?.scriptURL;
+        const registration = await Promise.race([
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            navigator.serviceWorker.ready,
+            new Promise((_, reject) => setTimeout(() => reject(new Error('Service worker registration failed: time out')), 10000))
+        ])
+        return registration.active?.scriptURL
     });
     const swName = generateSW ? 'sw.js' : 'prompt-sw.js'
     expect(swURL).toBe(`http://localhost:4173/${swName}`);
@@ -30,6 +33,11 @@ test('The service worker registers and precaches', async ({ page}) => {
 
     const urls = cacheContents[key].map(url => url.slice('http://localhost:4173/'.length))
 
+    /*
+    'http://localhost:4173/about?__WB_REVISION__=38251751d310c9b683a1426c22c135a2',
+    'http://localhost:4173/?__WB_REVISION__=073370aa3804305a787b01180cd6b8aa',
+    'http://localhost:4173/manifest.webmanifest?__WB_REVISION__=27df2fa4f35d014b42361148a2207da3'
+    */
     expect(urls.some(url => url.startsWith('manifest.webmanifest?__WB_REVISION__='))).toEqual(true)
     expect(urls.some(url => url.startsWith('?__WB_REVISION__='))).toEqual(true)
     expect(urls.some(url => url.startsWith('about?__WB_REVISION__='))).toEqual(true)
