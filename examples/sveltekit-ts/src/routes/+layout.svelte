21d06ea95b04cd5552bc67dea765e9034eaf8671
Jacob Bowdoin <7559478+jacob-8@users.noreply.github.com>
2023-06-21 08:27:31 +0800

    Easier component lazy-load method

--- examples/sveltekit-ts/src/routes/+layout.svelte
+++ examples/sveltekit-ts/src/routes/+layout.svelte
@@ -1,16 +1,9 @@
 <script lang="ts">
 	import Header from '$lib/header/Header.svelte';
 	import '../app.css';
-	import { onMount } from 'svelte';
 	import { pwaInfo } from 'virtual:pwa-info';
 
-	let ReloadPrompt;
-	onMount(async () => {
-		pwaInfo && (ReloadPrompt = (await import('$lib/ReloadPrompt.svelte')).default);
-	})
-
 	$: webManifest = pwaInfo ? pwaInfo.webManifest.linkTag : ''
-
 </script>
 
 <svelte:head>
@@ -27,9 +20,9 @@
 	<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
 </footer>
 
-{#if ReloadPrompt}
-	<svelte:component this={ReloadPrompt} />
-{/if}
+{#await import('$lib/ReloadPrompt.svelte') then { default: ReloadPrompt}}
+	<ReloadPrompt />
+{/await}
 
 <style>
 	main {
